c0151c0f6f71c13b5cac686369297f358eb2a2f5
Sebastien Valat <sebastien.valat@atos.net>
2021-02-23 17:40:50 +0100

    Fix minor bug in main.cpp

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -37,7 +37,7 @@ int main(int argc, const char ** argv)
 	
 	//info
 	printf("LISTEN: %s\n", config.listenIP.c_str());
-	if (config.nvdimmMountPath.empty()) {
+	if (config.nvdimmMountPath.empty() == false) {
 		for (int i = 0 ; i < config.nvdimmMountPath.size() ; i++)
 			printf("NVDIMM ENABLED: %s\n", config.nvdimmMountPath[i].c_str());
 		Object::setNvdimm(config.nvdimmMountPath);
