c438202cb76dd55eb06f374aa7043487b88f915f
Sebastien Valat <sebastien.valat@atos.net>
2021-03-31 18:19:29 +0200

    [nvdimm] Add a round robin memory backend to support multiple nvdimm mount points

---
+++ src/server/backends/MemoryBackendRoundRobin.cpp
@@ -0,0 +1,101 @@
+/*****************************************************
+			PROJECT  : IO Catcher
+			LICENSE  : Apache 2.0
+			COPYRIGHT: 2020 Bull SAS
+*****************************************************/
+
+/****************************************************/
+//internal
+#include "base/common/Debug.hpp"
+#include "MemoryBackendRoundRobin.hpp"
+
+/****************************************************/
+using namespace IOC;
+
+/****************************************************/
+MemoryBackendRoundRobin::MemoryBackendRoundRobin(void)
+	:MemoryBackend(NULL)
+{
+}
+
+/****************************************************/
+MemoryBackendRoundRobin::~MemoryBackendRoundRobin(void)
+{
+	//warn if not all zero
+	if (this->backendOfMem.empty() == false)
+		IOC_WARNING_ARG("Delete backends but one have memory : %1 !").arg(this->backendOfMem.size()).end();
+
+	//delete all backends
+	for (auto & it : this->backends)
+		delete it;
+}
+
+/****************************************************/
+void MemoryBackendRoundRobin::registerBackend(MemoryBackend * backend)
+{
+	this->backends.push_back(backend);
+	this->backendMem.push_back(0);
+}
+
+/****************************************************/
+void * MemoryBackendRoundRobin::allocate(size_t size)
+{
+	//check if has at least one
+	assert(this->backends.size() > 0);
+
+	//set to 0
+	size_t id = 0;
+	size_t min = this->backendMem[id];
+
+	//search min
+	for (int i = 1 ; i < this->backends.size() ; i++) {
+		if (this->backendMem[i] < min) {
+			id = i;
+			min = this->backendMem[i];
+		}
+	}
+
+	//send request to this one
+	void * ptr = this->backends[id]->allocate(size);
+	this->backendMem[id] += size;
+
+	//register
+	this->backendOfMem[ptr] = id;
+
+	//return 
+	return ptr;
+}
+
+/****************************************************/
+void MemoryBackendRoundRobin::deallocate(void * addr, size_t size)
+{
+	//check
+	assert(addr != NULL);
+	assert(size > 0);
+
+	//search
+	auto it = this->backendOfMem.find(addr);
+
+	//not found
+	assumeArg(it != this->backendOfMem.end(), "Fail to bound backend of the given memory : %1 !").arg(addr).end();
+
+	//free
+	size_t id = it->second;
+	this->backends[id]->deallocate(addr, size);
+
+	//erase & decrement
+	this->backendOfMem.erase(it);
+	assert(this->backendMem[id] >= size);
+	this->backendMem[id] -= size;
+}
+
+/****************************************************/
+size_t MemoryBackendRoundRobin::getMem(int id) const
+{
+	//check
+	assert(id >= 0);
+	assert(id < this->backendMem.size());
+
+	//return
+	return this->backendMem[id];
+}
---
+++ src/server/backends/MemoryBackendRoundRobin.hpp
@@ -0,0 +1,49 @@
+/*****************************************************
+			PROJECT  : IO Catcher
+			LICENSE  : Apache 2.0
+			COPYRIGHT: 2020 Bull SAS
+*****************************************************/
+
+#ifndef IOC_MEMORY_BACKEND_ROUND_ROBIN_HPP
+#define IOC_MEMORY_BACKEND_ROUND_ROBIN_HPP
+
+/****************************************************/
+//std
+#include <vector>
+#include <map>
+//internal
+#include "../core/MemoryBackend.hpp"
+
+/****************************************************/
+namespace IOC
+{
+
+/****************************************************/
+/**
+ * Implement a round robin memory backend permitting
+ * to distribute the allocations over several backends
+ * by keeping a balance over the memory size allocated
+ * on each one.
+**/
+class MemoryBackendRoundRobin: public MemoryBackend
+{
+	public:
+		MemoryBackendRoundRobin(void);
+		virtual ~MemoryBackendRoundRobin(void);
+		void registerBackend(MemoryBackend * backend);
+		size_t getMem(int id) const;
+	public:
+		virtual void * allocate(size_t size);
+		virtual void deallocate(void * addr, size_t size);
+	private:
+		/** Keep track of all the sub backends. **/
+		std::vector<MemoryBackend *> backends;
+		/** Keep track of the memory space used on each backend. **/
+		std::vector<size_t> backendMem;
+		/** Keep track to which backend the address belong. **/
+		std::map<void*, int> backendOfMem;
+};
+
+}
+
+#endif //IOC_MEMORY_BACKEND_ROUND_ROBIN_HPP
