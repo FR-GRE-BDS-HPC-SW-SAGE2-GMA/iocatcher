38225372fe659816a623768d1fd86ce4feb2c860
Sebastien Valat <sebastien.valat@atos.net>
2021-03-31 15:08:14 +0200

    [nvdimm] Define a memory backend bstract class

---
+++ src/server/core/MemoryBackend.cpp
@@ -0,0 +1,32 @@
+/*****************************************************
+			PROJECT  : IO Catcher
+			LICENSE  : Apache 2.0
+			COPYRIGHT: 2020 Bull SAS
+*****************************************************/
+
+/****************************************************/
+//internal
+#include "MemoryBackend.hpp"
+
+/****************************************************/
+using namespace IOC;
+
+/****************************************************/
+/**
+ * Basic constructor of the memory backend.
+ * @param lfDomain Define the libfabric domain to be used for memory
+ * registration after allocating new memory.
+**/
+MemoryBackend::MemoryBackend(LibfabricDomain * lfDomain)
+{
+	this->lfDomain = lfDomain;
+}
+
+/****************************************************/
+/**
+ * Virtual destructor for inheritance. Currently do nothing.
+**/
+MemoryBackend::~MemoryBackend(void)
+{
+	//nothing to do
+}
---
+++ src/server/core/MemoryBackend.hpp
@@ -0,0 +1,41 @@
+/*****************************************************
+			PROJECT  : IO Catcher
+			LICENSE  : Apache 2.0
+			COPYRIGHT: 2020 Bull SAS
+*****************************************************/
+
+#ifndef IOC_MEMORY_BACKEND_HPP
+#define IOC_MEMORY_BACKEND_HPP
+
+/****************************************************/
+//std
+#include <cstdlib>
+//internal
+#include "Object.hpp"
+#include "base/network/LibfabricDomain.hpp"
+
+/****************************************************/
+namespace IOC
+{
+
+/****************************************************/
+/**
+ * A memory backend handle the memory allocations of memory segments.
+ * There can be two implementation, a malloc base one and one for
+ * nvdimms.
+**/
+class MemoryBackend
+{
+	public:
+		MemoryBackend(LibfabricDomain * lfDomain);
+		virtual ~MemoryBackend(void);
+		virtual void * allocate(size_t size) = 0;
+		virtual void * deallocate(void * addr, size_t size) = 0;
+	protected:
+		/** Keep track of the libfabric domain for memory registration/deregistration. **/
+		LibfabricDomain * lfDomain;
+};
+
+}
+
+#endif //IOC_MEMORY_BACKEND_HPP
