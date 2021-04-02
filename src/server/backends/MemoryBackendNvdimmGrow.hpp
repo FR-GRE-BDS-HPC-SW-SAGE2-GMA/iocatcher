e053fd3956e02d8a5aeacdec9f01c4bc9f90d93e
Sebastien Valat <sebastien.valat@atos.net>
2021-04-02 11:41:42 +0200

    [nvdimm] Keep the grow based implementation in case we got a bugfix in the kernel

---
+++ src/server/backends/MemoryBackendNvdimmGrow.hpp
@@ -0,0 +1,56 @@
+/*****************************************************
+			PROJECT  : IO Catcher
+			LICENSE  : Apache 2.0
+			COPYRIGHT: 2020 Bull SAS
+*****************************************************/
+
+#ifndef IOC_MEMORY_BACKEND_NVDIMM_GROW_HPP
+#define IOC_MEMORY_BACKEND_NVDIMM_GROW_HPP
+
+/****************************************************/
+//std
+#include <string>
+#include <map>
+//internal
+#include "../core/MemoryBackend.hpp"
+
+/****************************************************/
+namespace IOC
+{
+
+/****************************************************/
+/**
+ * Implement memory backend using nvdimm memory. This class
+ * implement only the file allocation strategy which only grow
+ * the nvdimm file, it requires to be embedded into a 
+ * MemoryBackendCache to be efficient.
+ * This version of the implementation use ftunctate to grow
+ * the file for every new allocation.
+ *
+ * LIMITATION: There is currently a bug in the kernel which make
+ * ftruncate() blocked when the file mapped and pin memory
+ * for rdma communications.
+**/
+class MemoryBackendNvdimmGrow: public MemoryBackend
+{
+	public:
+		MemoryBackendNvdimmGrow(LibfabricDomain * lfDomain, const std::string & directory);
+		virtual ~MemoryBackendNvdimmGrow(void);
+		size_t getFileSize(void) const;
+	public:
+		virtual void * allocate(size_t size);
+		virtual void deallocate(void * addr, size_t size);
+	private:
+		/** directory in which to store the nvdimm data. **/
+		std::string directory;
+		/** The file descriptor handling the nvdimm file. **/
+		int fileFD;
+		/** Current size of the file. **/
+		size_t fileSize;
+		/** Count allocated chuncks **/
+		size_t chunks;
+};
+
+}
+
+#endif //IOC_MEMORY_BACKEND_NVDIMM_GROW_HPP
---
+++ src/server/backends/tests/TestMemoryBackendNvdimmGrow.cpp
@@ -0,0 +1,70 @@
+/*****************************************************
+			PROJECT  : IO Catcher
+			LICENSE  : Apache 2.0
+			COPYRIGHT: 2020 Bull SAS
+*****************************************************/
+
+/****************************************************/
+#include <gtest/gtest.h>
+#include "../MemoryBackendNvdimmGrow.hpp"
+#include <gmock/gmock.h>
+
+/****************************************************/
+using namespace IOC;
+using namespace testing;
+
+/****************************************************/
+TEST(TestMemoryBackendNvdimmGrow, allocate_deallocate_no_domain)
+{
+	//vars
+	const size_t size = 1024*1024;
+	MemoryBackendNvdimmGrow backend(NULL, "/tmp/");
+
+	//allocate
+	void * ptr = backend.allocate(size);
+	ASSERT_NE(nullptr, ptr);
+
+	//deallocate
+	backend.deallocate(ptr, size);
+}
+
+/****************************************************/
+TEST(TestMemoryBackendNvdimmGrow, allocate_deallocate_domain)
+{
+	//vars
+	const size_t size = 1024*1024;
+	LibfabricDomain domain("localhost", "82222", true);
+	MemoryBackendNvdimmGrow backend(&domain, "/tmp/");
+
+	//allocate
+	void * ptr = backend.allocate(size);
+	ASSERT_NE(nullptr, ptr);
+
+	//deallocate
+	backend.deallocate(ptr, size);
+}
+
+/****************************************************/
+TEST(TestMemoryBackendNvdimmGrow, allocate_deallocate_grow)
+{
+	//vars
+	const size_t size = 1024*1024;
+	LibfabricDomain domain("localhost", "82222", true);
+	MemoryBackendNvdimmGrow backend(&domain, "/tmp/");
+
+	//allocate
+	void * ptr = backend.allocate(size);
+	ASSERT_NE(nullptr, ptr);
+	ASSERT_EQ(size, backend.getFileSize());
+
+	//deallocate
+	backend.deallocate(ptr, size);
+
+	//allocate
+	void * ptr2 = backend.allocate(size);
+	ASSERT_NE(nullptr, ptr2);
+	ASSERT_EQ(2*size, backend.getFileSize());
+
+	//deallocate
+	backend.deallocate(ptr2, size);
+}
